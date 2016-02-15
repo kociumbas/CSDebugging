4a9b7dde6f76645c6e9e82ec62e6ec0778e8208a
kociumbas <kociumbas@outlook.com>
2016-02-16 00:27:00 +0100

    CSDebugging

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Debugging
+{
+    class Program
+    {
+        static private int Square(int argument)
+        {
+            int result;
+            result = checked(argument * argument);
+            if (result < 0)
+                throw new Exception("Square function should not return negative values!");
+            return result;
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x = 1234;
+                int y = Square(x);
+                y = Square(y);
+                string sy = y.ToString();
+                Console.WriteLine(sy);                
+            }
+            catch (Exception ex)
+            {
+                ConsoleColor currentColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Error.WriteLine("Error: " + ex.Message);
+                Console.ForegroundColor = currentColor;
+            }
+
+            
+        }
+    }
+}
